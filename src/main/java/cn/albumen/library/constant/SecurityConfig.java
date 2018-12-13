5cd3c7a4abd96a077770a82ecfa74ab0c6f7aa88
Albumen <jhq0812@hotmail.com>
2018-12-13 16:38:51 +0800

    fix:PEM文件修复

--- src/main/java/cn/albumen/library/constant/SecurityConfig.java
+++ src/main/java/cn/albumen/library/constant/SecurityConfig.java
@@ -4,6 +4,6 @@ package cn.albumen.library.constant;
  * @author Albumen
  */
 public class SecurityConfig {
-    public final static String PEM_KEY_PATH = "D:/Personal Work/library/Key/pem";
+    public final static String PEM_KEY_PATH = "D:/Personal Work/library_springboot/Key/pem";
     public final static Integer EXP_TIME = 60000000;
 }
