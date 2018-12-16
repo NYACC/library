ae04f71454cd074a730ee783b7a5316dfed239a6
Albumen <jhq0812@hotmail.com>
2018-12-16 23:01:02 +0800

    test:Add SQL file

--- sql/library.sql
+++ sql/library.sql
@@ -1,3 +1,19 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80011
+ Source Host           : localhost:3306
+ Source Schema         : library
+
+ Target Server Type    : MySQL
+ Target Server Version : 80011
+ File Encoding         : 65001
+
+ Date: 16/12/2018 23:00:51
+*/
+
 SET
 NAMES
 utf8mb4;
@@ -13,7 +29,7 @@ DROP TABLE IF EXISTS `book_category`;
 CREATE TABLE `book_category`
 (
   `id`   int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '分类名称',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -32,14 +48,14 @@ DROP TABLE IF EXISTS `book_detail`;
 CREATE TABLE `book_detail`
 (
   `id`             int(11) NOT NULL AUTO_INCREMENT,
-  `isbn`           varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `name`           varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `category_id`    int(11) NULL DEFAULT NULL,
-  `author`         varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `rent`           int(1) NULL DEFAULT NULL,
-  `rent_time`      datetime(0) NULL DEFAULT NULL,
-  `rent_back_time` datetime(0) NULL DEFAULT NULL,
-  `rent_user_id`   int(11) NULL DEFAULT NULL,
+  `isbn`           varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT 'ISBN',
+  `name`           varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '书名',
+  `category_id`    int(11) NULL DEFAULT NULL COMMENT '分类编号，对应user_category表中的id',
+  `author`         varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '作者',
+  `rent`           int(1) NULL DEFAULT NULL COMMENT '借出状态',
+  `rent_time`      datetime(0) NULL DEFAULT NULL COMMENT '借出时间',
+  `rent_back_time` datetime(0) NULL DEFAULT NULL COMMENT '预计归还时间',
+  `rent_user_id`   int(11) NULL DEFAULT NULL COMMENT '借出者',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -56,9 +72,9 @@ DROP TABLE IF EXISTS `library`;
 CREATE TABLE `library`
 (
   `id`         int(11) NOT NULL AUTO_INCREMENT,
-  `name`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `location`   varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `curator_id` int(11) NULL DEFAULT NULL,
+  `name`       varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '图书馆名称',
+  `location`   varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '图书馆位置',
+  `curator_id` int(11) NULL DEFAULT NULL COMMENT '馆长ID（对应user_security表中的id）',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -75,11 +91,11 @@ DROP TABLE IF EXISTS `log`;
 CREATE TABLE `log`
 (
   `id`         varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `user`       varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `ip`         varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `content`    varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `createTime` datetime(0) NULL DEFAULT NULL,
-  `level`      varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `user`       varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作用户',
+  `ip`         varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作ip',
+  `content`    varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作内容',
+  `createTime` datetime(0) NULL DEFAULT NULL COMMENT '操作时间',
+  `level`      varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '操作级别',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -90,10 +106,10 @@ DROP TABLE IF EXISTS `user_detail`;
 CREATE TABLE `user_detail`
 (
   `id`       int(11) NOT NULL AUTO_INCREMENT,
-  `user_id`  int(11) NOT NULL,
-  `sex`      int(1) NULL DEFAULT NULL,
-  `name`     varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `user_id`  int(11) NOT NULL COMMENT '用户id（对应user_security表中的id）',
+  `sex`      int(1) NULL DEFAULT NULL COMMENT '性别',
+  `name`     varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '姓名',
+  `location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '地址',
   PRIMARY KEY (`id`, `user_id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -112,11 +128,11 @@ DROP TABLE IF EXISTS `user_security`;
 CREATE TABLE `user_security`
 (
   `id`         int(11) NOT NULL AUTO_INCREMENT,
-  `user_no`    varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `phone`      varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `mail`       varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `password`   varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `permission` int(2) NULL DEFAULT NULL,
+  `user_no`    varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '学号/工号（可自定义，用户登录时的用户名）',
+  `phone`      varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '电话',
+  `mail`       varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '邮箱',
+  `password`   varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '密码',
+  `permission` int(2) NULL DEFAULT NULL COMMENT '权限',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `user_no`(`user_no`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 25 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
